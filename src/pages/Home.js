09030eb0399aca449384fde6f71a3b5b8399b61d
sofiane lasoa <sofiane.lasoa.etu@univ-lille.fr>
2022-11-30 11:57:00 +0100

    Fix problem

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,19 +6,17 @@ const Home = () => {
     return(
         <div className="home-container">
             <NavBar />
-            <div className="home">
-                <div className="home-content">
-                    <div className="home-content-top-container">
-                        Out.
+            <div className="home">  
+                    <div className="home-top-container">
+                        <span>Out.</span>
                     </div>
-                    <div className="home-content-bottom-container">
+                    <div className="home-bottom-container">
                         <h1>08°</h1>
                         <div>
                             <h2>London</h2>
                             <span>06:43 - mercredi 30 novembre</span>
                         </div>
                     </div>
-                </div>
             </div>
             
         </div>
