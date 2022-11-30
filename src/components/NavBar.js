06240f2ff652131254e085f86dae17857a0c77a6
sofiane lasoa <sofiane.lasoa.etu@univ-lille.fr>
2022-11-30 11:25:15 +0100

    Create Toogle navBar and update Home page responsive

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,19 +1,29 @@
 import React from "react";
 /* Icon */
 import SearchIcon from '@mui/icons-material/Search';
+import MenuIcon from '@mui/icons-material/Menu';
 
 const NavBar = () => {
     return(
-        <div className="nav-bar-container">
-            <div className="nav-bar-top">
-                <div className="nav-bar-top-search-container">
-                    <input type="text" placeholder="Location"></input>
+        <>
+            <div className="nav-bar-container">
+                <div className="nav-bar-top">
+                    <div className="nav-bar-top-search-container">
+                        <input type="text" placeholder="Location"></input>
+                    </div>
+                    <button>
+                        <SearchIcon />
+                    </button>
                 </div>
-                <button>
-                    <SearchIcon />
-                </button>
             </div>
-        </div>
+            <div className="toggle-nav-bar-container">
+                <div className="toogle-nav-bar-open">
+                    <MenuIcon />
+                </div>
+            </div>
+        
+        </>
+        
     )
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,6 +11,13 @@ const Home = () => {
                     <div className="home-content-top-container">
                         Out.
                     </div>
+                    <div className="home-content-bottom-container">
+                        <h1>08°</h1>
+                        <div>
+                            <h2>London</h2>
+                            <span>06:43 - mercredi 30 novembre</span>
+                        </div>
+                    </div>
                 </div>
             </div>
             
